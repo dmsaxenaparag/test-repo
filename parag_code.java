ba663739f8528ed16192c5fda13dd137bdf29ab8
Parag saxena <parag.saxena.94@gmail.com>
2016-07-07 11:14:21 +0530

    Update parag_code.java

--- parag_code.java
+++ parag_code.java
@@ -1,1 +1,14 @@
 
+package com.datametica.test;
+
+public class App {
+public static void main(String[] args) {
+int i = 0, j = 0;
+for (i=0; i<5; i++) {
+        for (j=0; j<5-i; j++) {
+                System.out.print("_");
+        }
+        System.out.println("*");
+        }
+}
+}
